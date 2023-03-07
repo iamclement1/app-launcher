2374a63719943ea1d2e955f1cc1e79f760854dc2
iamclement1 <blessedmarcel1@gmail.com>
2023-03-07 09:18:27 +0100

    landing + login page

---
+++ src/utils/dashboard.js
@@ -0,0 +1,38 @@
+import Icon from '../assets/Union.svg'
+export const DashboardCard = [
+    {
+        id: 1,
+        icon: Icon,
+        description: "Data Management System",
+    },
+    {
+        id: 2,
+        icon: Icon,
+        description: "Anniversary Alert Solutions",
+    },
+    {
+        id: 3,
+        icon: Icon,
+        description: "MTN Foundation App",
+    },
+    {
+        id: 4,
+        icon: Icon,
+        description: "Single Service Portal",
+    },
+    {
+        id: 5,
+        icon: Icon,
+        description: "After Sales Support Platform",
+    },
+    {
+        id: 6,
+        icon: Icon,
+        description: "Design Council",
+    },
+    {
+        id: 7,
+        icon: Icon,
+        description: "Amazon Tribe Pipeline",
+    }
+]
