ed290f26cd6a1a1d3e30e6d27073c9c8f8237475
iamclement1 <blessedmarcel1@gmail.com>
2023-03-07 14:42:12 +0100

    "home app filter"

---
+++ src/utils/appDashboard.js
@@ -0,0 +1,26 @@
+export const appHome = [
+    {
+        id: 1,
+        name: 'All'
+    },
+    {
+        id: 2,
+        name: 'Live'
+    },
+    {
+        id: 3,
+        name: 'UAT'
+    },
+    {
+        id: 4,
+        name: 'IAT'
+    },
+    {
+        id: 5,
+        name: 'Development'
+    },
+    {
+        id: 6,
+        name: 'Achieved'
+    }
+]
